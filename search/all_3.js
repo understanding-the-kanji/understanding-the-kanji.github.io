5e50747378dbe67b1a67a1cec38e79912e6f8f14
Samuel Mace <samuelcmace@gmail.com>
2022-11-05 11:07:03 -0500

    Update Documentation to Version 0.1.4

--- search/all_3.js
+++ search/all_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['editnotebutton_0',['editNoteButton',['../classnet_1_1samuelcmace_1_1utk_1_1gui_1_1controller_1_1KanjiViewController.html#a6d1ebeeec4c89da28945f7716251fd7a',1,'net::samuelcmace::utk::gui::controller::KanjiViewController']]],
-  ['error_1',['Error',['../classnet_1_1samuelcmace_1_1utk_1_1logic_1_1Logger.html#aa69933d26d8bca8d60ca07817c4bbb87',1,'net::samuelcmace::utk::logic::Logger']]]
+  ['enablefields_1',['enableFields',['../classnet_1_1samuelcmace_1_1utk_1_1gui_1_1controller_1_1KanjiViewController.html#a56f1a8f89d6f6cc2822244d1f0156692',1,'net::samuelcmace::utk::gui::controller::KanjiViewController']]],
+  ['error_2',['Error',['../classnet_1_1samuelcmace_1_1utk_1_1logic_1_1Logger.html#aa69933d26d8bca8d60ca07817c4bbb87',1,'net::samuelcmace::utk::logic::Logger']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['error_0',['Error',['../classnet_1_1samuelcmace_1_1utk_1_1logic_1_1Logger.html#aa69933d26d8bca8d60ca07817c4bbb87',1,'net::samuelcmace::utk::logic::Logger']]]
+  ['enablefields_0',['enableFields',['../classnet_1_1samuelcmace_1_1utk_1_1gui_1_1controller_1_1KanjiViewController.html#a56f1a8f89d6f6cc2822244d1f0156692',1,'net::samuelcmace::utk::gui::controller::KanjiViewController']]],
+  ['error_1',['Error',['../classnet_1_1samuelcmace_1_1utk_1_1logic_1_1Logger.html#aa69933d26d8bca8d60ca07817c4bbb87',1,'net::samuelcmace::utk::logic::Logger']]]
 ];
